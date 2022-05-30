ca497d222578d9b4eee82963a3d93516a26318bf
PaulBallestra <paulballestra@yahoo.fr>
2022-05-30 17:26:28 +0200

    Added Styles for Learning & Tools pages :construction:

--- src/features/learning/learning.screen.js
+++ src/features/learning/learning.screen.js
@@ -1,13 +1,28 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+import LinearGradient from 'react-native-linear-gradient';
+
+import styles from './learning.styles'
+import { COLORS } from '../../constants/themes'
+
 class Learning extends React.Component {
 
     render(){
         return (
-            <View>
-              <Text>Learning</Text>
+          <View style={styles.body}>
+              
+          <LinearGradient colors={[COLORS.backgroundTop, COLORS.backgroundBottom]} style={styles.linearGradient}>
+
+            <View style={styles.headerView}>
+              <Text style={styles.headerTitle}> LUCIDITY </Text>
+              <Text style={styles.headerSubTitle}> PRENEZ LE CONTRÔLE </Text>
             </View>
+
+        
+          </LinearGradient>
+          
+        </View>
           );
     }
 
--- src/features/tools/tools.screen.js
+++ src/features/tools/tools.screen.js
@@ -1,13 +1,28 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+import LinearGradient from 'react-native-linear-gradient';
+
+import styles from './tools.styles'
+import { COLORS } from '../../constants/themes'
+
 class Tools extends React.Component {
 
     render(){
         return (
-            <View>
-              <Text>Tools</Text>
+          <View style={styles.body}>
+              
+          <LinearGradient colors={[COLORS.backgroundTop, COLORS.backgroundBottom]} style={styles.linearGradient}>
+
+            <View style={styles.headerView}>
+              <Text style={styles.headerTitle}> LUCIDITY </Text>
+              <Text style={styles.headerSubTitle}> PRENEZ LE CONTRÔLE </Text>
             </View>
+
+        
+          </LinearGradient>
+          
+        </View>
           );
     }
 
